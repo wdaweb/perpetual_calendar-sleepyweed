77db41898c85ce51388ea1f4cd7e7b6609d0f19b
sleepyweed <marumaru19960115@gmail.com>
2020-11-10 15:26:46 +0800

    基本版型完成

--- MyCalender.php
+++ MyCalender.php
@@ -7,31 +7,56 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://kit.fontawesome.com/97604f290f.js" crossorigin="anonymous"></script>
   <style>
   @import url('https://fonts.googleapis.com/css2?family=Fredericka+the+Great&display=swap');
   body{
     font-family:'Fredericka the Great', cursive;
-    font-size:34px;
+    font-size:36px;
     color:#eee;
     background: url('https://i.imgur.com/hy1Kv6g.jpg') no-repeat;
     background-size:100%;
-    position: fixed;
-    left: 50%;
-    top: 50%;
-    transform:translate(-50%,-50%);
+
+    margin: 50px;
+    padding: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 90vh;
   }
   .title{
     font-family:'Fredericka the Great', cursive;
-    font-size:72px;
+    font-size:78px;
     display:inline-block;
+  }
+  .left,.right{
+    display:inline-block;
+    animation: button 4s linear infinite ;
+  }
+  @keyframes button{
+    0%,100%{
+      filter:drop-shadow(0 0 12px lightyellow);
+    }
+    25%{
+      filter:drop-shadow(0 0 2px rgb(51,51,51));
+    }
+    50%{
+      filter:drop-shadow(0 0 12px lightyellow);
+    }
+    75%{
+      filter:drop-shadow(0 0 2px rgb(51,51,51));
+    }
+}
 
+  .left{
+    position:absolute;
+    top:50%;
+    left:10%;
   }
-  .title2{
-    display:inline-block;
-  }
-  table{
-    height:500px;
-    /* margin-top:1rem; */
+  .right{
+    position:absolute;
+    top:50%;
+    right:10%;
   }
   table td{
     width:80px;
@@ -39,24 +64,28 @@
     padding:10px 0;
   }
   .s2:hover{
-    background:
+    background:url('https://i.imgur.com/wzpvL9l.png')no-repeat;
+    background-size:60%;
+    background-position:center bottom;
   }
   .s1{
-    color:lightseagreen;
+    color:rgb(254,125,182);
+    font-size:38px;
   }
   a:link{
-    color:white;
-    font-size:28px;
+    color:#eee;
+    font-size:72px;
   }
   a:visited{
-    color:white;
+    color:#eee;
   }
   a:hover{
-    color:lightcoral;
+    color:rgb(254,125,182);
+    opacity:0.8;
     text-decoration:none;
   }
   a:active{
-    color:white;
+    color:#eee;
   }
   </style>
 </head>
@@ -85,16 +114,15 @@ $FirstWeek=date('w',strtotime("$Year-$Month-01"));//③第一天星期幾
 // $LastWeek=date('w',strtotime(date("$Year-$Month-$Days")));//④最後一天星期幾
 $Weeks=(($Days+$FirstWeek-7)/7)+1;//這個月有幾週
 ?>
-<div class="title"><?=$Year;?></div>
-<div class="title"><?=$MonthTitle;?></div>
+<div class="title"><?=$Year;?>&nbsp;<?=$MonthTitle;?></div>
 <?php
 if(($Month-1)>0){
 ?>
-  <div class="title2"><a href="?year=<?=$Year;?>&month=<?=($Month-1);?>">Last Month</a></div>
+  <div class="left"><a href="?year=<?=$Year;?>&month=<?=($Month-1);?>"><i class="fas fa-caret-left"></i></a></div>
 <?php
 }else{
 ?>
-  <div class="title2"><a href="?year=<?=($Year-1);?>&month=<?=12;?>">Last Month</a></div>
+  <div class="left"><a href="?year=<?=($Year-1);?>&month=<?=12;?>"><i class="fas fa-caret-left"></i></a></div>
 <?php
 }
 ?>
@@ -102,15 +130,14 @@ if(($Month-1)>0){
 <?php
 if(($Month+1)<=12){
 ?>
-<a href="?year=<?=$Year;?>&month=<?=($Month+1);?>">Next Month</a>
+  <div class="right"><a href="?year=<?=$Year;?>&month=<?=($Month+1);?>"><i class="fas fa-caret-right"></i></a></div>
 <?php
 }else{
 ?>
-  <a href="?year=<?=($Year+1);?>&month=<?=1;?>">Next Month</a>
+  <div class="right"><a href="?year=<?=($Year+1);?>&month=<?=1;?>"><i class="fas fa-caret-right"></i></a></div>
 <?php
 }
 ?>
-
 <table>
 <tr class="s1">
   <td>Sun</td>
@@ -137,7 +164,6 @@ for($i=0;$i<$Weeks;$i++){
   }echo "</tr>";
 }
 ?>
-
 </table>
 </div>
 </body>
